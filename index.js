2d1bbe1def37a299acffff8d96d84c14d07afa06
Andrei Regiani <andrei.cpp@gmail.com>
2026-01-20 13:04:12 -0300

    lint format

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 'use strict'
-const extensions = new Set([
-  'html',
-  'js',
-  'json',
-  'md',
-  'mjs',
-  'txt'
-])
+const extensions = new Set(['html', 'js', 'json', 'md', 'mjs', 'txt'])
 module.exports = extensions
